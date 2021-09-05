49a20cc946efe8086b68300064ffccdfe260efb8
fkfkfk9 <fkfkfk999@gmail.com>
2021-09-05 23:19:58 +0900

    핵심역량 작성시작

--- _config.js
+++ _config.js
@@ -32,6 +32,6 @@ module.exports = {
   googleAnalytics: 'G-NHNE0QZVKG',     // Google Analytics TrackingID. ex.'UA-123456789-0'
   googleSearchConsole: '', // content value in HTML tag of google search console ownership verification. ex.'w-K42k14_I4ApiQKuVPbCRVV-GxlrqWxYoqO94KMbKo'
   googleAdsenseSlot: '',   // Google Adsense Slot. ex.'5214956675'
-  googleAdsenseClient: '', // Google Adsense Client. ex.'ca-pub-5001380215831339'
+  googleAdsenseClient: 'ca-pub-5143454690757819', // Google Adsense Client. ex.'ca-pub-5001380215831339'
     // Please correct the adsense client number(ex.5001380215831339) in the './static/ads.txt' file.
 };
